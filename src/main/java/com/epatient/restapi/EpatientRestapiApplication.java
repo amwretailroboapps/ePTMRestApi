1be63ce6e356abee669f97909357099354368e22
amwretailroboapps <retail.robo.app@gmail.com>
2024-09-09 20:01:16 +0530

    update the data

--- src/main/java/com/epatient/restapi/EpatientRestapiApplication.java
+++ src/main/java/com/epatient/restapi/EpatientRestapiApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EpatientRestapiApplication {
 		
 	public static void main(String[] args) {
-		//testing changes
+		//main class to run
 		SpringApplication.run(EpatientRestapiApplication.class, args);
 	}
 
