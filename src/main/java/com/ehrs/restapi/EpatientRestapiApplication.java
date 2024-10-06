175bc326e5f68438ce3f2fac888bf3dba1d272e9
amwretailroboapps <retail.robo.app@gmail.com>
2024-10-06 11:49:59 +0530

    getFirst changes

--- src/main/java/com/ehrs/restapi/EpatientRestapiApplication.java
+++ src/main/java/com/ehrs/restapi/EpatientRestapiApplication.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+//@Configuration
+//@ComponentScan
+//@EnableAutoConfiguration
 @SpringBootApplication
 public class EpatientRestapiApplication extends SpringBootServletInitializer{
 
